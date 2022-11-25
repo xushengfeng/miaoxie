080e45ca996c9e9a364cefa1c5234b938e9a9dff
xushengfeng <xushengfeng_zg@163.com>
2022-11-25 23:08:19 +0800

    主要 聚焦传信号

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import { appWindow } from "@tauri-apps/api/window";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
 async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+    if (greetMsgEl && greetInputEl) {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        greetMsgEl.textContent = await invoke("greet", {
+            name: greetInputEl.value,
+        });
+    }
 }
+await appWindow.onFocusChanged(async ({ payload: focused }) => {
+    console.log("Focus changed, window is focused? " + focused);
+    greet();
+});
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
+    greetInputEl = document.querySelector("#greet-input");
+    greetMsgEl = document.querySelector("#greet-msg");
+    document.querySelector("#greet-button")?.addEventListener("click", () => greet());
 });
